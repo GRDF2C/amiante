a73a1af854c004ec383323887b033a9995b67826
GRDF2C <grdf-idf-dirreseaux-ai-slt@grdf.fr>
2025-09-17 11:13:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
 const formUrl = "https://prev2s.grdf.fr/form/choice";
 
-const interv1Url  = "https://grdf2c.github.io/ville-grdf/";
-const interv2Url  = "https://google.com";
+const interv1Url  = "https://grdf.sharepoint.com/teams/Prevention_Sante_Securite_de_la_DR_IDF/Maitrise%20des%20risques/Forms/AllItems.aspx?viewid=961d002f%2D11d6%2D46fc%2D912e%2D198da788eb23&FolderCTID=0x0120006FFC4DDA3A4CF443BC5A9747F653DF17&id=%2Fteams%2FPrevention%5FSante%5FSecurite%5Fde%5Fla%5FDR%5FIDF%2FMaitrise%20des%20risques%2FChimique%20%28amiante%2C%20CO%2C%2E%2E%2E%29%2FAmiante%2FModes%20Op%C3%A9ratoires%20%2D%20Vid%C3%A9os%2FAmiante%2C%20Mode%20Op%C3%A9ratoire%20%2D%20MO%2001%2C%20Per%C3%A7age%20de%20parois%20%C3%A0%20vitesse%20la%20%2B%20r%C3%A9duite%20possible%20avec%20humidification%2Epdf&parent=%2Fteams%2FPrevention%5FSante%5FSecurite%5Fde%5Fla%5FDR%5FIDF%2FMaitrise%20des%20risques%2FChimique%20%28amiante%2C%20CO%2C%2E%2E%2E%29%2FAmiante%2FModes%20Op%C3%A9ratoires%20%2D%20Vid%C3%A9os";
+const interv2Url  = "https://grdf.sharepoint.com/teams/Prevention_Sante_Securite_de_la_DR_IDF/Maitrise%20des%20risques/Forms/AllItems.aspx?viewid=961d002f%2D11d6%2D46fc%2D912e%2D198da788eb23&FolderCTID=0x0120006FFC4DDA3A4CF443BC5A9747F653DF17&id=%2Fteams%2FPrevention%5FSante%5FSecurite%5Fde%5Fla%5FDR%5FIDF%2FMaitrise%20des%20risques%2FChimique%20%28amiante%2C%20CO%2C%2E%2E%2E%29%2FAmiante%2FModes%20Op%C3%A9ratoires%20%2D%20Vid%C3%A9os%2FAmiante%2C%20Mode%20Op%C3%A9ratoire%20%2D%20MO%2001bis%2C%20Per%C3%A7age%20traversant%20de%20parois%20%C3%A0%20vitesse%20la%20plus%20r%C3%A9duite%20possible%20%C3%A0%20travers%20un%20SAS%20de%20gel%20hydrique%20%2Epdf&parent=%2Fteams%2FPrevention%5FSante%5FSecurite%5Fde%5Fla%5FDR%5FIDF%2FMaitrise%20des%20risques%2FChimique%20%28amiante%2C%20CO%2C%2E%2E%2E%29%2FAmiante%2FModes%20Op%C3%A9ratoires%20%2D%20Vid%C3%A9os";
 const interv3Url  = "";
 const interv4Url  = "";
 const interv5Url  = "";

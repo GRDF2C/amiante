38e9bb30ab513760207805a79e25f7f4010299a8
GRDF2C <grdf-idf-dirreseaux-ai-slt@grdf.fr>
2025-10-02 11:58:50 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 
 const formUrl = "https://prev2s.grdf.fr/form/choice";
 
-const interv1Url  = "https://grdf.sharepoint.com/teams/Prevention_Sante_Securite_de_la_DR_IDF/Maitrise%20des%20risques/Forms/AllItems.aspx?viewid=961d002f%2D11d6%2D46fc%2D912e%2D198da788eb23&FolderCTID=0x0120006FFC4DDA3A4CF443BC5A9747F653DF17&id=%2Fteams%2FPrevention%5FSante%5FSecurite%5Fde%5Fla%5FDR%5FIDF%2FMaitrise%20des%20risques%2FChimique%20%28amiante%2C%20CO%2C%2E%2E%2E%29%2FAmiante%2FModes%20Op%C3%A9ratoires%20%2D%20Vid%C3%A9os%2FAmiante%2C%20Mode%20Op%C3%A9ratoire%20%2D%20MO%2001%2C%20Per%C3%A7age%20de%20parois%20%C3%A0%20vitesse%20la%20%2B%20r%C3%A9duite%20possible%20avec%20humidification%2Epdf&parent=%2Fteams%2FPrevention%5FSante%5FSecurite%5Fde%5Fla%5FDR%5FIDF%2FMaitrise%20des%20risques%2FChimique%20%28amiante%2C%20CO%2C%2E%2E%2E%29%2FAmiante%2FModes%20Op%C3%A9ratoires%20%2D%20Vid%C3%A9os";
-const interv2Url  = "https://grdf.sharepoint.com/teams/Prevention_Sante_Securite_de_la_DR_IDF/Maitrise%20des%20risques/Forms/AllItems.aspx?viewid=961d002f%2D11d6%2D46fc%2D912e%2D198da788eb23&FolderCTID=0x0120006FFC4DDA3A4CF443BC5A9747F653DF17&id=%2Fteams%2FPrevention%5FSante%5FSecurite%5Fde%5Fla%5FDR%5FIDF%2FMaitrise%20des%20risques%2FChimique%20%28amiante%2C%20CO%2C%2E%2E%2E%29%2FAmiante%2FModes%20Op%C3%A9ratoires%20%2D%20Vid%C3%A9os%2FAmiante%2C%20Mode%20Op%C3%A9ratoire%20%2D%20MO%2001bis%2C%20Per%C3%A7age%20traversant%20de%20parois%20%C3%A0%20vitesse%20la%20plus%20r%C3%A9duite%20possible%20%C3%A0%20travers%20un%20SAS%20de%20gel%20hydrique%20%2Epdf&parent=%2Fteams%2FPrevention%5FSante%5FSecurite%5Fde%5Fla%5FDR%5FIDF%2FMaitrise%20des%20risques%2FChimique%20%28amiante%2C%20CO%2C%2E%2E%2E%29%2FAmiante%2FModes%20Op%C3%A9ratoires%20%2D%20Vid%C3%A9os";
+const interv1Url  = "";
+const interv2Url  = "";
+const interv2_1Url = "";
+const interv2_2Url = "";
 const interv3Url  = "";
+const interv3_1Url = "";
+const interv3_2Url = "";
+const interv3_3Url = "";
+const interv3_4Url = "";
 const interv4Url  = "";
 const interv5Url  = "";
 const interv6Url  = "";
@@ -11,11 +17,7 @@ const interv7Url  = "";
 const interv8Url  = "";
 const interv9Url  = "";
 const interv10Url = "";
-const interv11Url = "";
-const interv12Url = "";
-const interv13Url = "";
-const interv14Url = "";
-const interv15Url = "";
+
 
 
 const btnForm = document.getElementById("btnForm");
@@ -74,11 +76,16 @@ btnToggleMenu.addEventListener("click", () => {
 
 
 const urlById = {
-  interv1:  interv1Url,  interv2:  interv2Url,  interv3:  interv3Url,
-  interv4:  interv4Url,  interv5:  interv5Url,  interv6:  interv6Url,
-  interv7:  interv7Url,  interv8:  interv8Url,  interv9:  interv9Url,
-  interv10: interv10Url, interv11: interv11Url, interv12: interv12Url,
-  interv13: interv13Url, interv14: interv14Url, interv15: interv15Url,
+  interv1:  interv1Url,
+  interv2:  interv2Url,  interv2_1:  interv2_1Url,  interv2_2:  interv2_2Url,
+  interv3:  interv3Url,  interv3_1:  interv2_1Url,  interv3_2:  interv2_2Url,  interv3_3:  interv3_4Url,
+  interv4:  interv4Url,
+  interv5:  interv5Url,
+  interv6:  interv6Url,
+  interv7:  interv7Url,
+  interv8:  interv8Url,
+  interv9:  interv9Url,
+  interv10: interv10Url,
 };
 
 list.addEventListener("click", (e) => {
@@ -88,3 +95,30 @@ list.addEventListener("click", (e) => {
   const url = urlById[id];
   if (url) window.open(url, "_blank");
 });
+
+// Gestion des sous-menus
+function toggleSubmenu(btnId, submenuId) {
+  const btn = document.getElementById(btnId);
+  const submenu = document.getElementById(submenuId);
+
+  btn.addEventListener("click", (e) => {
+    e.stopPropagation(); // évite d’ouvrir un lien direct
+    const expanded = btn.getAttribute("aria-expanded") === "true";
+
+    // Fermer si déjà ouvert
+    if (expanded) {
+      btn.setAttribute("aria-expanded", "false");
+      submenu.hidden = true;
+    } else {
+      // Ferme les autres sous-menus ouverts
+      document.querySelectorAll(".submenu").forEach(s => s.hidden = true);
+      document.querySelectorAll(".menu-item.has-arrow").forEach(b => b.setAttribute("aria-expanded", "false"));
+
+      btn.setAttribute("aria-expanded", "true");
+      submenu.hidden = false;
+    }
+  });
+}
+
+toggleSubmenu("interv2", "submenu-interv2");
+toggleSubmenu("interv3", "submenu-interv3");
